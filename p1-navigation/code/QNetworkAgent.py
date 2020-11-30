7db56160c7960c655fb75fe5c13b437207064eb2
Francesco Torregrossa <d.ftorregrossa@gmail.com>
2020-11-30 15:04:46 +0100

    add comments to QNetworkAgent

--- p1-navigation/code/QNetworkAgent.py
+++ p1-navigation/code/QNetworkAgent.py
@@ -7,12 +7,59 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class QNetworkAgent():
+    """DQN agent. Keeps two networks, q_local (policy) and q_target (estimation of TD target).
+       Learning is performed automatically after a certain number of calls to store function."""
 
     def __init__(self, QNetwork, state_size, action_size,
                  replay_buffer, Delta,
-                 eps=1, eps_decay=0.9995, min_eps=0.0001, gamma=0.99,
-                 alpha=0.001, tau=0.01,
+                 eps=1, eps_decay=0.9995, min_eps=0.0001,
+                 gamma=0.99, alpha=0.001, tau=0.01,
                  update_every=15, batch_size=64, learning=True):
+        """Create a new agent initialized with random values.
+
+        Parameters
+        ----------
+            QNetwork : class
+                One of DQN or DuelingDQN. Will be used to create two instances of the given class.
+            state_size : int
+                Number of parameters in the state.
+            action_size : int
+                Number of actions available.
+            replay_buffer : UniformReplayBuffer
+                Instance of UniformReplayBuffer. Will be used to store and sample experiences.
+            Delta : function
+                One of dt_dqn or dt_double_dqn. Will be used to calculate the error between
+                the estimated TD target (by q_target) and the current value (by q_local).
+                Will be used to train q_local using backpropagation.
+            eps : float
+                Initial value for the epsilon-greedy policy. The value needs to be in [0, 1], where 1
+                means that every action taken by act() is random and 0 means that every action is greedy.
+            eps_decay : float
+                Used to decrease eps at every learning step. eps_decay needs to be in [0, 1].
+            min_eps : float
+                eps will not go below this value during training. Updates are performed according to
+                eps = max(eps * eps_decay, min_eps).
+            gamma : float
+                Weight of the estimation of TD target, used in Delta. The value needs to be in [0, 1]
+                (usually closer to 1) where 1 means that future rewards are as important as the immediate 
+                reward and 0 means that future rewards are ignored.
+            alpha : float
+                Learning rate of the optimizer used for q_local. 
+                The value needs to be in [0, 1] (usually closer to 0).
+            tau : float
+                We copy the parameters of q_local in q_target after a learning step. tau allows for 'soft'
+                updates, where instead of a direct copy of q_local we interpolate towards it, according to
+                q_target = (1 - tau) q_target + tau (q_local). The value needs to be in [0, 1] (usually closer to 0),
+                where 1 means that we directly copy the parameters and 0 means that q_target stays constant.
+            update_every : int
+                Indicates how many experiences to store before performing a learning step.
+                Note that new experiences aren't guaranteed to be sampled.
+            batch_size : int
+                Number of tuples to be sampled in a single learning step.
+            learning : bool
+                Determines if the policy used is eps-greedy (True) or greedy (False).
+        """
+
         self.state_size, self.action_size = state_size, action_size
         self.original_eps = eps
         self.QNetwork = QNetwork
@@ -25,6 +72,8 @@ class QNetworkAgent():
         self.reset()
 
     def reset(self):
+        """Delete experiences, recreate q_local and q_target and reset eps to its original value."""
+
         self.replay_buffer.reset()
         self.eps = self.original_eps
         self.q_local = self.QNetwork(
@@ -35,6 +84,18 @@ class QNetworkAgent():
         self.update_i = 0
 
     def act(self, s):
+        """Choose an action according to eps, learning, and q_local.
+
+        Parameters
+        ----------
+        s : torch.tensor
+            Tensor of size [state_size]
+
+        Returns
+        -------
+            int representing the action that is likely to maximize cumulative rewards,
+            according to eps-greedy/greedy policy estimated by q_local.
+        """
 
         # eps-greedy policy: decide if the action should be random or greedy
         # (always greedy if agent.learning = False)
@@ -49,6 +110,22 @@ class QNetworkAgent():
             return np.random.randint(self.action_size)
 
     def store(self, s, a, r, ns, d):
+        """Add a new experience to the replay buffer. Automatically calls learn() when needed.
+
+        Parameters
+        ----------
+        s : torch.tensor
+            Initial state of the environment
+        a : int
+            Action taken by the agent
+        r : float
+            Reward obtained for taking that action
+        ns : torch.tensor
+            State of the environment after taking that action
+        d : bool
+            Whether or not the episode is over
+        """
+
         # store a new experience
         self.replay_buffer.add((s, a, r, ns, d))
 
@@ -64,7 +141,9 @@ class QNetworkAgent():
         self.update_i = (self.update_i + 1) % self.update_every
 
     def learn(self):
-        # note that this is called automatically by the agent
+        """Sample a batch of experiences (based on batch_size), use Delta to estimate the error
+        and take a gradient step to minimize it in q_local. Then use q_local to perform a soft 
+        update of q_target. Note that this is called automatically by the agent."""
 
         # sample tuples of experiences from memory (each of these variables is a torch tensor)
         s, a, r, ns, d = self.replay_buffer.sample(self.batch_size)
--- p1-navigation/code/dt_estimators.py
+++ p1-navigation/code/dt_estimators.py
@@ -6,27 +6,26 @@ def dt_dqn(s, a, r, ns, d, q_local, q_target, gamma):
 
     Parameters
     ----------
-        s : torch.tensor
-            Current state (size [n * m], where n is the batch size and m is the number of features in the state)
-        a : torch.tensor
-            Action taken (size [n])
-        r : torch.tensor
-            Reward obtained (size [n])
-        ns : torch.tensor
-            Next state (size [n * m])
-        d : torch.tensor
-            True if the episode ended after the action (size [n])
-        q_local : torch.nn.Module
-            Network used to determine the policy
-        q_target : torch.nn.Module
-            Copy of q_local that is updated less frequently
-        gamma : float
-            Weight of the estimation of future rewards, in range [0, 1]
+    s : torch.tensor
+        Current state (size [n * m], where n is the batch size and m is the number of features in the state)
+    a : torch.tensor
+        Action taken (size [n])
+    r : torch.tensor
+        Reward obtained (size [n])
+    ns : torch.tensor
+        Next state (size [n * m])
+    d : torch.tensor
+        True if the episode ended after the action (size [n])
+    q_local : torch.nn.Module
+        Network used to determine the policy
+    q_target : torch.nn.Module
+        Copy of q_local that is updated less frequently
+    gamma : float
+        Weight of the estimation of future rewards, in range [0, 1]
 
     Returns
     -------
-        torch.tensor 
-            Tensor of size [n] where each row is the delta_t of a tuple
+        torch.tensor of size [n] where each row is the delta_t of a tuple
     """
     with torch.no_grad():
         QT = q_target(ns).max(1)[0]
@@ -39,27 +38,26 @@ def dt_double_dqn(s, a, r, ns, d, q_local, q_target, gamma):
 
     Parameters
     ----------
-        s : torch.tensor
-            Current state (size [n * m], where n is the batch size and m is the number of features in the state)
-        a : torch.tensor
-            Action taken (size [n])
-        r : torch.tensor
-            Reward obtained (size [n])
-        ns : torch.tensor
-            Next state (size [n * m])
-        d : torch.tensor
-            True if the episode ended after the action (size [n])
-        q_local : torch.nn.Module
-            Network used to determine the policy
-        q_target : torch.nn.Module
-            Copy of q_local that is updated less frequently
-        gamma : float
-            Weight of the estimation of future rewards, in range [0, 1]
+    s : torch.tensor
+        Current state (size [n * m], where n is the batch size and m is the number of features in the state)
+    a : torch.tensor
+        Action taken (size [n])
+    r : torch.tensor
+        Reward obtained (size [n])
+    ns : torch.tensor
+        Next state (size [n * m])
+    d : torch.tensor
+        True if the episode ended after the action (size [n])
+    q_local : torch.nn.Module
+        Network used to determine the policy
+    q_target : torch.nn.Module
+        Copy of q_local that is updated less frequently
+    gamma : float
+        Weight of the estimation of future rewards, in range [0, 1]
 
     Returns
     -------
-        torch.tensor 
-            Tensor of size [n] where each row is the delta_t of a tuple
+        torch.tensor of size [n] where each row is the delta_t of a tuple
     """
     with torch.no_grad():
         QLns = q_local(ns).max(1)[1].unsqueeze(1)
